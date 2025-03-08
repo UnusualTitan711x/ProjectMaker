9473368341bb41e9c7de36de600c7d953c6ccf71
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-03-08 09:53:42 +0100

    improved web project creation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
+import click
+import yaml
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
 # # Use os.chdir to change the working directory, and os.mkdir to create a directory
 # os.chdir(r"C:\Users\Arthur Djiomou\Github\ProjectMaker")
@@ -9,48 +14,55 @@ import os
 
 def main():
     print("Welcome to Project Maker.")
+    print("Please make sure you have cd to the directory you want your project in.")
     print("What type of project will it be?")
     print("1. Web project\n2. Godot Project")
     choice = int(input("Choice: "))
 
     match choice:
         case 1:
-            CreateWebProject()
-
-def CreateWebProject():
-    project_name = input("What is the name of the project: ")
-    dir = input("Please enter the directory for the project: ")
-    os.chdir(dir)
-
-    if os.path.exists(f"{dir}\{project_name}"):
-        pass
-    else:
-        os.mkdir(project_name)
-
-    html_file = open(f"{dir}\{project_name}\index.html", "w+")
-    css_file = open(f"{dir}\{project_name}\style.css", "w+")
-    js_file = open(f"{dir}\{project_name}\script.js", "w+")
-
-    html_template = """<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{project_name}</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
+            create_web()
         
-        <script src="script.js"></script>
-    </body>
-    </html>"""
 
-    html_file.write(html_template)
+def create_web():
+    project_name = input("What is the name of the project: ")
 
-    html_file.close()
-    css_file.close()
-    js_file.close()    
+    project_path = os.path.join(os.getcwd(), project_name)
+    
+    # Make sure to control which directory to use
+    if not os.path.exists(project_path):
+        os.makedirs(project_path)
+    else:
+        click.echo("Directory already exists.")
+        return    
 
-    print("Process Completed!")
+    for file in config["project_templates"]["web"]["files"]:
+        file_path = os.path.join(project_path, file)
+
+        with open(file_path, "w+") as f:
+            if file == "index.html":
+                html_template = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{project_name}</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <script src="script.js"></script>
+</body>
+</html>"""
+
+                f.write(html_template)
+            elif file == "README.md":
+                f.write(f"{project_name} - Web Project")
+
+            f.close()
+
+    click.echo("Process Completed!")
+
+
 
 main()
